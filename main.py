c7869843a1f6bfd7b7e62c13a0dc965cbe1b44bc
pophubup <Yohoo0987@gmail.com>
2021-09-18 19:00:54 +0800

    test3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_restx import Resource, Api, fields
 from utils import DATOInforLoader
 app = Flask(__name__)
 cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app, version='1.0', title='WebCrawling',
     description='Crawling everyWhere For Fun',)
 
@@ -17,7 +18,7 @@ MY_MODEL = ns.model("Result",
     },
 )
 @cross_origin()
-@ns.route('/Dato/<string:name>')
+@ns.route('/<string:name>')
 class DATO(Resource):
     @ns.param('name', '玩家名稱')
     @ns.response(404, '找不到此玩家')
