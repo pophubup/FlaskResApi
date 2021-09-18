88c9327cec0a884262475727acc1df4dc9dc82d2
pophubup <Yohoo0987@gmail.com>
2021-09-18 18:39:33 +0800

    inital

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify,make_response
+from flask_cors import CORS, cross_origin
+from flask_restx import Resource, Api, fields
+from utils import DATOInforLoader
+app = Flask(__name__)
+cors = CORS(app)
+api = Api(app, version='1.0', title='WebCrawling',
+    description='Crawling everyWhere For Fun',)
+
+
+
+ns = api.namespace('DATO', description='DATO 遊戲者戰績')
+MY_MODEL = ns.model("Result",
+    {
+        "page": fields.String(example="some string"),
+        "result": fields.List(fields.String)
+    },
+)
+@cross_origin()
+@ns.route('/Dato/<string:name>')
+class DATO(Resource):
+    @ns.param('name', '玩家名稱')
+    @ns.response(404, '找不到此玩家')
+    @ns.response(200, "OK", MY_MODEL) 
+    def get(self, name):
+        status_code, result= DATOInforLoader(name, r'C:\Users\Yohoo\Desktop\myAllTest\zWebCrawlingRepository\pys\data.json', 5).DataAsJsonData()
+        return make_response(jsonify(result),status_code)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from time import sleep
+import json
+
+class DATOInforLoader():
+    def __init__(self, username, filepath, totalpage):
+        self.url = "https://twlolstats.com/summoner/?summoner={}".format(
+            username)
+        self.path = filepath
+        self.totalpage = totalpage
+
+    def __CreateDriver(self):
+        driver = webdriver.Edge(
+            executable_path=r"C:\Users\Yohoo\Downloads\msedgedriver.exe")
+        driver.maximize_window()
+        driver.get(self.url)
+        str1 = driver.title
+       
+        if str1 == 'Server Error (500)':
+            return (404, "Not Found Resource")
+         
+        return (200 ,driver)
+    def __GetData(self):
+        status, driver = self.__CreateDriver()
+        if status == 404:
+            return (404, driver)
+        gather2 = []
+        x = [i for i in range(2, self.totalpage) if i != self.totalpage]
+        for i in x:
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            sleep(1)
+            str = '#game{} input'.format(i)
+            driver.find_elements_by_css_selector(str)[0].click()
+            sleep(1)
+            str = '#game{} table tbody td:nth-child(3) div:nth-child(1) b'.format(
+                i)
+            isWin = driver.find_elements_by_css_selector(str)
+            str2 = '#game{} table tbody td:nth-child(3) div:nth-child(2)'.format(
+                i)
+            Kind = driver.find_elements_by_css_selector(str2)
+            str3 = '#game{} table tbody td:nth-child(3) div:nth-child(3)'.format(
+                i)
+            FinshTime = driver.find_elements_by_css_selector(str3)
+            str4 = '#game{} table tbody td:nth-child(3) div:nth-child(4)'.format(
+                i)
+            TotalHour = driver.find_elements_by_css_selector(str4)
+            final1 = [{"index": g + 1, "isWinOrFail": isWin[g].text, "type": Kind[g].text,
+                       "finishTime": FinshTime[g].text, "totalPlayHour": TotalHour[g].text} for g, v in enumerate(isWin)]
+            gather2.append({"page": i, "result": final1})
+            isWin = driver.find_elements_by_css_selector(
+                'table:nth-child(2) tbody td:nth-child(3) div:nth-child(1) b')
+            Kind = driver.find_elements_by_css_selector(
+                'table:nth-child(2) tbody td:nth-child(3) div:nth-child(2)')
+            FinshTime = driver.find_elements_by_css_selector(
+                'table:nth-child(2) tbody td:nth-child(3) div:nth-child(3)')
+            TotalHour = driver.find_elements_by_css_selector(
+                'table:nth-child(2) tbody td:nth-child(3) div:nth-child(4)')
+            final1 = [{"index": g + 1, "isWinOrFail": isWin[g].text, "type": Kind[g].text,
+                       "finishTime": FinshTime[g].text, "totalPlayHour": TotalHour[g].text} for g, v in enumerate(isWin)]
+            gather2.insert(0, {"page": 1, "result": final1})
+        return (200 , gather2)
+
+    def DataAsJsonData(self):
+        return self.__GetData()
+
+    def DataAsJsonPhysicalFile(self):
+        state, result = self.__GetData()
+        if state == 404:
+            return "No Resource Found"
+        with open(self.path, 'w') as outfile:
+            json.dump(result, outfile, sort_keys=True, indent=4,
+                      ensure_ascii=False)
+        return "Save Finished"
+
+
+# obj = DATOInforLoader(
+#    "fdsfds", r'C:\Users\Yohoo\Desktop\myAllTest\zWebCrawlingRepository\pys\data.json', 10)
+
+# print(obj.DataAsJsonPhysicalFile())
